3e59591514fed233378da942723bbed5c15895b7
Abhishek <abhishek1015@gmail.com>
2021-11-13 17:15:01 -0500

    adding change in configuration

--- code/launch_multitask_training_job.sh
+++ code/launch_multitask_training_job.sh
@@ -10,7 +10,7 @@ learning_rate=0.00005
 num_patch=32
 batch_size=12
 recon_size=64
-dropout=0.8
+dropout=0.5
 kl_coeff=0
 
 sbatch --gres=gpu:p100:4 --time=10:00:00 train_multitask_distributed.job $batch_size $num_patch $patch_size $recon_size $warmup_k $modelarch $learning_rate $dropout $kl_coeff $latent_dim $stat_norm_scheme $reference_patch
