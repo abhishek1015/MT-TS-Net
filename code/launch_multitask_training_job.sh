d883390cec00bac853490aef1b72a53521017712
Abhishek <abhishek1015@gmail.com>
2021-11-13 16:49:43 -0500

    adding script

---
+++ code/launch_multitask_training_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+patch_size=224
+modelarch="\"resnet18\""
+latent_dim=512
+description="\"brca_multitask_experiment\""
+stat_norm_scheme="pretrained"
+reference_patch="reference_patch_224.pkl"
+learning_rate=0.00001
+num_patch=32
+batch_size=8
+recon_size=64
+dropout=0.8
+kl_coeff=0
+
+sbatch --gres=gpu:p100:4 --time=10:00:00 train_multitask_distributed.job $batch_size $num_patch $patch_size $recon_size $warmup_k $modelarch $learning_rate $dropout $kl_coeff $latent_dim $stat_norm_scheme $reference_patch
+
